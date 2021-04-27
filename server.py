7bd24352e4a11aadc390267ddc29975e4379df95
gigamarr <gigamarr@protonmail.com>
2021-04-27 17:47:56 +0400

    updated logging

--- server.py
+++ server.py
@@ -1,16 +1,12 @@
 import os
 import socketserver
 import requests
-import logging
-from datetime import datetime
 from dotenv import load_dotenv
 from matcher import match_protocol
 from exceptions import ProtocolNotRecognized
+from logs import debug_logger, error_logger, critical_logger
 
-# FIXME: DEBUG level logs from requests logger getting logged anyways, probably because it gets reset on line 12
 
-logging.getLogger('requests').setLevel(logging.WARNING)
-logging.basicConfig(filename='exceptions.log', level=logging.DEBUG)
 load_dotenv()
 
 socketserver.ThreadingTCPServer.allow_reuse_address = True
@@ -27,23 +23,27 @@ class TCPHandler(socketserver.BaseRequestHandler):
         try:
             self.initial_data = self.request.recv(1024).decode()
             self.protocol_object = match_protocol(self.initial_data)
-            print(self.protocol_object.payload)
             self.post_data(data=self.protocol_object.payload)
         except ProtocolNotRecognized:
             if len(self.initial_data) == 0:
-                logging.debug(f'Empty data sent in by a client. {self.client_address} | {datetime.now().strftime("%Y, %B %d %H:%M:%S")}')
+                debug_logger.debug(f'Empty data sent in by client {self.client_address}')
             else:
-                logging.warning(f'Unrecognized protocol: {self.initial_data} | {datetime.now().strftime("%Y, %B %d %H:%M:%S")}')
+                error_logger.error(f'Unrecognized protocol {self.initial_data}')
         except Exception as e:
-            logging.critical(f'Uncaught exception: {e} | {datetime.now().strftime("%Y, %B %d %H:%M:%S")}')
+            critical_logger.critical(f'Uncaught exception: {e}')
 
     def post_data(self, data: dict) -> None:
         r = requests.post(self.server.BACKEND_ADDRESS, data=data)
-        print(r.text)
+
+        if r.status_code != 201:
+            error_logger.error(f'Server returned unexpected response: {r.text}')
 
 
 if __name__ == "__main__":
     server_address = ('', 8090)
 
     with CustomThreadingTCPServer(server_address, TCPHandler) as server:
-        server.serve_forever()
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt, closing the station.')
