872bc6ba8280f80d3abda17909a28e49140e8212
Giga <aqari.team@gmail.com>
2021-06-25 22:32:51 +0400

    supercharged station with async/await and refactored to keep the connections alive

--- server.py
+++ server.py
@@ -10,77 +10,54 @@ from logs import debug_logger, error_logger, critical_logger
 
 load_dotenv()
 
-class ClientContext:
-    """
-    Class that handles requests, uses one session for all mobile devices that connect to this station,
-    mainly here just so I don't have to re-create a session and re-load BACKEND_URL environment variable
-    each time a client connects.
-    """
 
-    def __init__(self):
-        self.counter = 0
-        self.session = aiohttp.ClientSession()
-        self.server_address = os.getenv('BACKEND_URL')
-    
+class Station:
+
+    def __init__(self, session: aiohttp.ClientSession, server_address: str) -> None:
+        self.session = session
+        self.server_address = server_address
+
     async def send_to_server(self, payload: dict) -> None:
-        """
-        Sends clean payload ready for server to aqari main server for saving.
-
-        :param payload: Dictionary containing key,value pair of field name and actual value for the server API to save
-        :type payload: dict
-        :rtype: None
-        """
         response = await self.session.post(self.server_address, data=payload)
 
-        if response.status != 201:
+        if response.status == 201:
+            # Here server should any commands that might be there in queue for the
+            pass
+        else:
             error_logger.error(f'Server returned unexpected response: {response.text}')
-    
+
+
     async def handle_request(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-        """
-        Fires once data is sent in by a mobile station (GPS), identifies protocol or raises an exception,
-        fires method to send the data to server in case everything goes well.
 
-        :param reader: Represents a reader object that provides APIs to read data from the IO stream.
-        :type reader: asyncio.StreamReader
-        :param writer: Represents a writer object that provides APIs to write data to the IO stream.
-        :type writer: asyncio.StreamWriter
-        :raises ProtocolNotRecognized: On occasions where protocol is not recognized (includes when sometimes client sends empty data)
-        :raises Exception: Other unforeseen exceptions
-        :rtype: None
-        """
-        initial_data = await reader.read(1024)
+        while True:
+            initial_data = await reader.read(1024)
+            initial_data = initial_data.decode()
 
-        initial_data = initial_data.decode()
+            print(initial_data)
 
-        print(f'Received: {initial_data}')
+            try:
+                protocol_object = match_protocol(initial_data)
+                await self.send_to_server(protocol_object.payload)
+            except ProtocolNotRecognized:
+                if initial_data:
+                    error_logger.error(f'Unrecognized protocol {initial_data}')
+                else:
+                    debug_logger.debug(f'Empty data sent in by a client. Closing connection now.')
+                    writer.close()
+                    await writer.wait_closed()
+                    break
+            except Exception as e:
+                critical_logger.critical(f'Uncaught exception: {e}')
 
-        try:
-            protocol_object = match_protocol(initial_data)
-            response = await self.send_to_server(protocol_object.payload)
-        except ProtocolNotRecognized:
-            if len(initial_data) == 0:
-                debug_logger.debug(f'Empty data sent in by a client.')
-            else:
-                error_logger.error(f'Unrecognized protocol: {initial_data}')
-        except Exception as e:
-            critical_logger.critical(f'Uncaught exception: {e}')
-
-
-        response = f"*HQ,9171718264,R12,{protocol_object.payload['time']}#"
-        print(response)
-        writer.write(response.encode())
-        await writer.drain()
-
-        writer.close()
-        await writer.wait_closed()
-
+            
 
 async def main():
-    ctx = ClientContext()
+    session = aiohttp.ClientSession()
+    server_address = os.getenv('BACKEND_URL')
 
-    server = await asyncio.start_server(
-        ctx.handle_request, '', 8090
-    )
+    station = Station(session, server_address)
+
+    server = await asyncio.start_server(station.handle_request, '', 8090)
 
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
