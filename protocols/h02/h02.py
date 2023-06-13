1abef02e547c6bdd9a7d1f451638b8851a9176bf
aqari-dev <aqari.team@gmail.com>
2023-06-13 16:40:50 +0400

    master: update h02 regexp to match new shitty devices

--- protocols/h02/h02.py
+++ protocols/h02/h02.py
@@ -19,8 +19,8 @@ class H02(BaseProtocol):
 
     # TODO: test that all protocols define those with protocol name appended in front for dictionary key
     regular_expressions: dict = {
-        r'^\*([A-Z]+),(\d{10}),(V\d),(\d{6}),(A|B|V),(-?\d{4}.\d{4}),(N|S),(-?\d{5}.\d{4}),(E|W),(\d{1,3}.\d{2}),(\d{1,3}),(\d{6}),([0-9A-Fa-f]+),(\d+),(\d+),(\d+),(\d+)(,\d+)?#$': Location,
-        r'^\*([A-Z]+),(\d{10}),(V\d),(S\d+),(OK|DONE),(\d{6}),(\d{6}),(A|B|V),(-?\d{4}.\d{4}),(N|S),(-?\d{5}.\d{4}),(E|W),(\d{1,3}.\d{2}),(\d{1,3}),(\d{6}),([0-9A-Fa-f]+),(\d+),(\d+),(\d+),(\d+)(,\d+)?#$': Location,
+        r'^\*([A-Z]+),(\d{10}),(V\d),(\d{6}),(A|B|V),(-?\d{4}.\d{4}),(N|S),(-?\d{4,5}.\d{4}),(E|W),(\d{1,3}.\d{2}),(\d{1,3}),(\d{6}),([0-9A-Fa-f]+),(\d+),(\d+),(\d+),(\d+)(,\d+)?#$': Location,
+        r'^\*([A-Z]+),(\d{10}),(V\d),(S\d+),(OK|DONE),(\d{6}),(\d{6}),(A|B|V),(-?\d{4}.\d{4}),(N|S),(-?\d{4,5}.\d{4}),(E|W),(\d{1,3}.\d{2}),(\d{1,3}),(\d{6}),([0-9A-Fa-f]+),(\d+),(\d+),(\d+),(\d+)(,\d+)?#$': Location,
         r'^H02,(\d{10}),(CUT_FUEL|ENABLE_FUEL)$': Command,
         # For testing
         r'custom\((\d{10})\)\((.*)\)': CustomCommand
