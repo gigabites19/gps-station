f381696552474579d075e1762a09c66b68607579
aqari-dev <aqari.team@gmail.com>
2023-07-02 19:27:58 +0400

    v2.0: add class docstring to h02 protocol implementation

--- protocols/h02/h02.py
+++ protocols/h02/h02.py
@@ -1,7 +1,10 @@
 from protocols import BaseProtocol
+
 from .packet_decoder import H02PacketDecoder
 
 class H02Protocol(BaseProtocol):
+    """Implementation of H02 protocol, mostly used by ST-901 trackers by SinoTrack."""
+
     @property
     def packet_decoder(self) -> H02PacketDecoder:
         return H02PacketDecoder()
