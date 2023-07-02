b7da3dc6957885f77d175f50c1b3166f8ffad7aa
aqari-dev <aqari.team@gmail.com>
2023-07-02 22:44:22 +0400

    v2.0: move project source code into station directory

--- exceptions.py
+++
@@ -1,5 +0,0 @@
-class ProtocolNotRecognized(Exception):
-
-    def __init__(self, data: str, message='Protocol was not recognized'):
-        self.message = f'{message}: {data}'
-        super().__init__(self.message)
