ceff2a82c7b3ce56893c013f636176086f62d8d8
aqari-dev <aqari.team@gmail.com>
2023-07-02 18:48:36 +0400

    v2.0: add custom exception module

---
+++ protocols/exceptions.py
@@ -0,0 +1,7 @@
+"""Module for exceptions that can occur when working with various protocols."""
+
+
+class RegExMatchError(Exception):
+    """Regular expression has no match in a piece of data."""
+    pass
+
