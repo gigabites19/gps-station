ef3ff17c1c53da9f6a626f513b23bdd936201605
aqari-dev <aqari.team@gmail.com>
2023-07-02 19:22:44 +0400

    v2.0: add new kind of exception

--- protocols/exceptions.py
+++ protocols/exceptions.py
@@ -1,7 +1,19 @@
 """Module for exceptions that can occur when working with various protocols."""
 
 
+# TODO: display pattern that did not match data
 class RegExMatchError(Exception):
     """Regular expression has no match in a piece of data."""
+    pass
+
+
+# TODO: display data that that caused this error
+class BadProtocolError(Exception):
+    """Protocol data structured in an unexpected way.
+
+    This error either means that station has incorrect expectations from the protocol, or
+    a malicious actor is connecting by sending a valid protocol data packet at first and then sending packets with arbitrary data.
+    Or the device itself is misbehaving.
+    """
     pass
 
