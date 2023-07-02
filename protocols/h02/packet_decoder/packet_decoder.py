0560624c0d87846f975ec5e4a661843b6802d9f2
aqari-dev <aqari.team@gmail.com>
2023-07-02 19:23:22 +0400

    v2.0: decode h02 data packets in  block

--- protocols/h02/packet_decoder/packet_decoder.py
+++ protocols/h02/packet_decoder/packet_decoder.py
@@ -1,15 +1,23 @@
 from protocols.packet_decoder import BasePacketDecoder
 
+from protocols.exceptions import RegExMatchError, BadProtocolError
+
 from protocols.h02.payloads import H02Location
 
 from .decoders import decode_h02_ascii_packet, decode_h02_binary_packet
 
 
 class H02PacketDecoder(BasePacketDecoder):
+
     def decode(self, raw_bytes: bytes) -> H02Location:
-        if raw_bytes.startswith(b'*'):
-            return decode_h02_ascii_packet(raw_bytes)
-        elif raw_bytes.startswith(b'$'):
-            return decode_h02_binary_packet(raw_bytes)
-        # TODO: raise an exception
+        try:
+            if raw_bytes.startswith(b'*'):
+                return decode_h02_ascii_packet(raw_bytes)
+            elif raw_bytes.startswith(b'$'):
+                return decode_h02_binary_packet(raw_bytes)
+            else:
+                # TODO: log this
+                raise BadProtocolError
+        except (UnicodeDecodeError, RegExMatchError, BadProtocolError):
+            raise
 
