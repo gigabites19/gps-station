a11f617886acf6cf517d6c7a1ed3943836bfeaaf
aqari-dev <aqari.team@gmail.com>
2023-07-02 21:07:55 +0400

    v2.0: add docstring to H02PacketDecoder decode method

--- protocols/h02/packet_decoder/packet_decoder.py
+++ protocols/h02/packet_decoder/packet_decoder.py
@@ -9,6 +9,20 @@ from .decoders import decode_h02_ascii_packet, decode_h02_binary_packet
 class H02PacketDecoder(BasePacketDecoder):
 
     def decode(self, raw_bytes: bytes) -> H02Location:
+        """Decode location packet sent by a device using H02 protocol.
+
+        Args:
+            raw_bytes:
+                Raw bytes of the packets.
+
+        Returns:
+            `H02Location` object that contains all required data to be saved in the backend.
+
+        Raises:
+            BadProtocolError: If data packet begins unexpectedly (Neither '*' nor '$').
+            UnicodeDecodeError: If ASCII mode can not be decoded properly.
+            RegExMatchError: If one of the decoders fail to decode a certain parameter (they use regular expressions internally).
+        """
         try:
             if raw_bytes.startswith(b'*'):
                 return decode_h02_ascii_packet(raw_bytes)
@@ -16,6 +30,6 @@ class H02PacketDecoder(BasePacketDecoder):
                 return decode_h02_binary_packet(raw_bytes)
             else:
                 raise BadProtocolError(f'Expected H02 packet to start with either "*" or "$" instead got (bytes): {list(raw_bytes)}')
-        except (UnicodeDecodeError, RegExMatchError, BadProtocolError):
+        except (BadProtocolError, UnicodeDecodeError, RegExMatchError):
             raise
 
