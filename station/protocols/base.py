f2311945d34d87cf9a6de1b8ded22a9e596d5496
aqari-dev <aqari.team@gmail.com>
2023-07-07 18:33:11 +0400

    v2.0: define packet_decoder as class attribute

--- station/protocols/base.py
+++ station/protocols/base.py
@@ -10,32 +10,24 @@ class BaseProtocol(ABC):
     """Blueprint for all other protocols to build upon.
 
     Meant to provide a common interface for concrete implementations of various GPS protocols.
-    `packet_decoder` must be defined as a property because it is a mandatory
-    attribute for `BaseProtocol` subclass' initialization and python does not provide a way to forbid
-    class initialization if it does not implement certain attributes, but it does provide a way
-    to do that for methods using `abstractmethod` decorator.
  
     Attributes:
+        packet_decoder:
+            instance of `BasePacketDecoder`'s subclass used to decode data packets sent by device.
         stream_writer:
             `asyncio.StreamWriter` instance used to send data to device.
         stream_reader:
             `asyncio.StreamReader` instance used to await and read data sent by devices.
-        packet_decoder:
-            instance of `BasePacketDecoder`'s subclass used to decode data packets sent by device.
         session:
             Aiohttp session shared by all `BaseProtocol` instances, used to send data uplink.
     """
+
+    packet_decoder: BasePacketDecoder
 
     def __init__(self, stream_reader: asyncio.StreamReader, stream_writer: asyncio.StreamWriter, session: aiohttp.ClientSession) -> None:
         self.stream_reader = stream_reader
         self.stream_writer = stream_writer
         self.client_session = session
-
-    @property
-    @abstractmethod
-    def packet_decoder(self) -> BasePacketDecoder:
-        """Return instance of `BasePacketDecoder`'s subclass."""
-        pass
 
     @staticmethod
     @abstractmethod
